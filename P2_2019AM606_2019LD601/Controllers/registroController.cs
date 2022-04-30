55df97e1f87ede028feac5835dd97dcfaaa49c13
Andrés Enrique Lemus Deleón <andres.lemus@catolica.edu.sv>
2022-04-30 13:07:49 -0600

    DB Context y Connection String

---
+++ P2_2019AM606_2019LD601/Controllers/registroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2_2019AM606_2019LD601.Controllers
+{
+    public class registroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ P2_2019AM606_2019LD601/dbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace P2_2019AM606_2019LD601
+{
+    public class dbContext : DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options)
+        {
+
+        }
+
+        /*public DbSet<CasosReportados> regiones { get; set; }
+        public DbSet<Departamentos> departamentos { get; set; }
+        public DbSet<Generos> registros { get; set; }*/
+    }
+}
