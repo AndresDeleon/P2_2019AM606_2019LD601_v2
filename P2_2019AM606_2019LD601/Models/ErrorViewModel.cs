dd2b80afd2a85ca461663f0b3eafe1bc4a7de255
Andrés Enrique Lemus Deleón <andres.lemus@catolica.edu.sv>
2022-04-30 12:36:04 -0600

    Primera Cargar

---
+++ P2_2019AM606_2019LD601/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace P2_2019AM606_2019LD601.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
